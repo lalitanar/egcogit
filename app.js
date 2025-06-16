933a2a8736e4a33308abb90b06ade75f078a4a74
lalitanar <lalita.nar@mahidol.edu>
2025-06-16 18:53:29 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.json({ message: 'Hello World' }))
+app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
 
 app.listen(3000, () => console.log('server start'))
 
